09035dc93d46dd113893e822d6982b79de78bded
Alana Kerr <ani.kerr@gmail.com>
2019-03-13 14:29:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   let lowerCase = string.toLowerCase();
+  let upperCase = string.toUpperCase();
+  
   if (string === lowerCase) {
     return "I can't hear you!";
   }
+  
+  if (string === upperCase) {
+    return "YES INDEED!";
+  }
+  
+  if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
+
